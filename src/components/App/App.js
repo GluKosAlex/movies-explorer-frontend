0f8e863be2e1abe498fa7e51a66786d815b2526e
GluKosAlex <glukosalex@gmail.com>
2024-01-02 18:51:11 +0700

    feat(app): add css color variables

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import logo from '../../images/logo.svg';
 import './App.css';
+import Preloader from './../Preloader/Preloader';
 
 function App() {
   return (
     <>
+      <Preloader />
     </>
   );
 }
