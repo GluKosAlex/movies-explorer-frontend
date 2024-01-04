a1b3dc154a672b39e209aa2951eb5c2040944939
GluKosAlex <glukosalex@gmail.com>
2024-01-04 22:47:13 +0700

    feat(app): create header markup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+
+import Navigation from './../Navigation/Navigation';
+import logo from './../../images/logo.svg';
+import './Header.css';
 
 export default function Header() {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Главная</Link>
-          </li>
-          <li>
-            <NavLink to="movies">Фильмы</NavLink>
-          </li>
-          <li>
-            <NavLink to="saved-movies">Сохранённые фильмы</NavLink>
-          </li>
-          <li>
-            <Link to="profile">Аккаунт</Link>
-          </li>
-          <li>
-            <Link to="signin">Войти</Link>
-          </li>
-          <li>
-            <Link to="signup">Регистрация</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <header className="header">
+      <img className="header__logo" src={logo} alt="Логотип сайта Movie Explorer" />
+      <Navigation />
+    </header>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,40 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Главная</NavLink>
+          </li>
+          <li>
+            <NavLink to="movies">Фильмы</NavLink>
+          </li>
+          <li>
+            <NavLink to="saved-movies">Сохранённые фильмы</NavLink>
+          </li>
+          <li>
+            <Link to="profile">Аккаунт</Link>
+          </li>
+          <li>
+            <Link to="signin">Войти</Link>
+          </li>
+          <li>
+            <Link to="signup">Регистрация</Link>
+          </li>
+        </ul>
+      </nav>
+      <button
+        className="menu__toggle menu__toggle_closed"
+        onClick={(e) => {
+          e.target.classList.toggle('menu__toggle_closed');
+          e.target.parentNode.classList.toggle('header__menu_closed');
+        }}
+      >
+        <span>Открыть меню</span>
+      </button>
+    </>
+  );
 }
