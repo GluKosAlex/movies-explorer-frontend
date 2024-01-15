a1e104b169d83a2541ee1e9453ee85944f52a21e
GluKosAlex <glukosalex@gmail.com>
2024-01-16 01:29:31 +0700

    refactor(movie): rewrite search form with react hook form

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './FilterCheckbox.css';
 
-export default function FilterCheckbox({ isChecked, onSwitchState, className: classList = '' }) {
+export default function FilterCheckbox({ register, name, className: classList = '', ...props }) {
   return (
     <div className={`${classList} show-shorts`}>
       <input
+        {...register(name)}
         type="checkbox"
         id="show-shorts"
-        name="show-shorts"
         className="show-shorts__checkbox"
-        checked={isChecked}
-        onChange={() => onSwitchState(!isChecked)}
+        {...props}
       />
       <label htmlFor="show-shorts" className="show-shorts__label">
         Короткометражки
