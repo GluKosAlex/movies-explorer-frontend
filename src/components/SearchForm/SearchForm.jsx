a3f9d3f0d07c46aa20e7f7a32593961897e0ebc9
GluKosAlex <glukosalex@gmail.com>
2024-01-20 23:20:19 +0700

    feat(movie): add validation to search form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,8 +4,11 @@ import { useContext } from 'react';
 import FilterCheckbox from './../FilterCheckbox/FilterCheckbox';
 import MyInput from './../ui/MyInput/MyInput';
 import MyButton from './../ui/MyButton/MyButton';
+import { MoviesFilterContext } from '././../../contexts/MoviesFilterContext';
+import { validationOptions } from './../../constants/validationOptions';
 import './SearchForm.css';
-import { MoviesFilterContext } from './../../contexts/MoviesFilterContext';
+
+const { movieSearchValidOptions } = validationOptions;
 
 export default function SearchForm() {
   const { moviesFilter, setMoviesFilter } = useContext(MoviesFilterContext);
@@ -13,8 +16,13 @@ export default function SearchForm() {
   const methods = useForm({
     defaultValues: { search: '', isShort: false },
     value: { search: moviesFilter.query, isShort: moviesFilter.isShort },
+    mode: 'onSubmit',
   });
-  const { handleSubmit, register } = methods;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = methods;
 
   const searchFormSubmitHandler = (data) => {
     setMoviesFilter({ query: data.search, isShort: data.isShort });
@@ -27,11 +35,13 @@ export default function SearchForm() {
         <fieldset className="search-form__fieldset">
           <MyInput
             register={register}
+            registerOptions={movieSearchValidOptions}
             name="search"
             type="search"
             className="search-form__input"
             placeholder="Фильм"
           />
+          <span className={`search-form__input-error`}>{errors?.['search']?.message}</span>
           <MyButton className="search-form__btn">Найти</MyButton>
         </fieldset>
         <FilterCheckbox name={'isShort'} register={register} />
