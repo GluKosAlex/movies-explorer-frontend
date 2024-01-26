bab6e635cd2e176e956bf907484f39d5a4307f6e
GluKosAlex <glukosalex@gmail.com>
2024-01-26 22:56:42 +0700

    fix(app): fix protected route redirect

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute({ children, ...props }) {
-  return props.loggedIn ? children : <Navigate to="/signin" replace />;
+  return props.loggedIn ? children : <Navigate to="/" replace />;
 }
